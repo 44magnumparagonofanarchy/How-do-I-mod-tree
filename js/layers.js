f8af1a2d657df7bb6c15715be7032fc820b6e79f
Glitch (platinum-rhinestone-lace) <none>
2021-09-18 22:53:31 +0000

    🌮💠 Checkpoint ./js/layers.js:32268312/14390

--- js/layers.js
+++ js/layers.js
@@ -183,73 +183,81 @@ addLayer("E", {
       }
     }
   },
-tabFormat: {
-    "Questions": { // these are the names of the subtabs, whatever you put here is what appears on the button
-        content: [
-                "main-display",
-    "prestige-button",
-    "blank",
-    "milestones",
-    "blank",
-    "blank",
-    "upgrades"// all the stuff for the actual layer you're using
-        ]
+  tabFormat: {
+    Questions: {
+      // these are the names of the subtabs, whatever you put here is what appears on the button
+      content: [
+        "main-display",
+        "prestige-button",
+        "blank",
+        "milestones",
+        "blank",
+        "blank",
+        "upgrades" // all the stuff for the actual layer you're using
+      ]
     },
-    "Answers": {
-        embedLayer: "Ghost",
+    Answers: {
+      embedLayer: "A",
       unlocked() {
-        return hasUpgrade("E", 52) || 
+        return hasUpgrade("E", 52) || hasUpgrade("A", 11);
       },
       content: [
-                "main-display",
-    "prestige-button",
-    "blank",
-    "milestones",
-    "blank",
-    "blank",
-    "upgrades"// all the stuff for the actual layer you're using        
+        "main-display",
+        "prestige-button",
+        "blank",
+        "milestones",
+        "blank",
+        "blank",
+        "upgrades" // all the stuff for the actual layer you're using
       ]
     }
-         }
-})
+  }
+});
 addLayer("A", {
-    startData() { return {                  // startData is a function that returns default data for a layer. 
-        unlocked: true,                   // You can add more variables here to add them to your layer.
-        points: new Decimal(1),             // "points" is the internal name for the main resource of the layer.
-    }},
+  startData() {
+    return {
+      // startData is a function that returns default data for a layer.
+      unlocked: true, // You can add more variables here to add them to your layer.
+      points: new Decimal(0) // "points" is the internal name for the main resource of the layer.
+    };
+  },
 
-    color: "#4BDC13",                       // The color for this layer, which affects many elements.
-    resource: "Answers",            // The name of this layer's main prestige resource.
-    row: 1000000,
+  color: "#4BDC13", // The color for this layer, which affects many elements.
+  resource: "Answers", // The name of this layer's main prestige resource.
+  row: 1000000,
   position: 1000,
-    baseResource: "E",                 // The name of the resource your prestige gain is based on.
-    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+  baseResource: "E", // The name of the resource your prestige gain is based on.
+  baseAmount() {
+    return player.points;
+  }, // A function to return the current amount of baseResource.
 
-    requires: new Decimal(1),              // The amount of the base needed to  gain 1 of the prestige currency.
-                                            // Also the amount required to unlock the layer.
+  requires: new Decimal(1), // The amount of the base needed to  gain 1 of the prestige currency.
+  // Also the amount required to unlock the layer.
 
-    type: "normal",                         // Determines the formula used for calculating prestige currency.
-    exponent: 0.5,                          // "normal" prestige gain is (currency^exponent).
+  type: "normal", // Determines the formula used for calculating prestige currency.
+  exponent: 0.5, // "normal" prestige gain is (currency^exponent).
 
-    gainMult() {                            // Returns your multiplier to your gain of the prestige resource.
-        return new Decimal(1)               // Factor in any bonuses multiplying gain here.
-    },
-    gainExp() {                             // Returns your exponent to your gain of the prestige resource.
-        return new Decimal(1)
-    },
+  gainMult() {
+    // Returns your multiplier to your gain of the prestige resource.
+    return new Decimal(1); // Factor in any bonuses multiplying gain here.
+  },
+  gainExp() {
+    // Returns your exponent to your gain of the prestige resource.
+    return new Decimal(1);
+  },
 
-    layerShown() { return false },          // Returns a bool for if this layer's node should be visible in the tree.
+  layerShown() {
+    return false;
+  }, // Returns a bool for if this layer's node should be visible in the tree.
 
-    upgrades: {
-            11: {
+  upgrades: {
+    11: {
       title: "Oh, it's called Javascript, not Java's Crypt.",
       description: "Answers ",
       cost: new Decimal(1),
       unlocked() {
-        return player[this.layer].unlocked;
+        return hasUpgrade("E", 52) || hasUpgrade("A", 11)
       }
-    },
-
-    },
-  
+    }
+  }
 });
